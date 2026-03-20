e2c0512e3da3cb04e894b4134e31c24580d73ddd
TNMV07 <trannammv@gmail.com>
2026-03-20 14:03:10 +0700

    Add files via upload

---
+++ chapter7.py
@@ -0,0 +1,6 @@
+print("Exercise 1- Min, Max, Average")
+import math
+readings=[23.1,26.4,21.8,29.3,25.0]
+print("The max value is: ",max(readings))
+print("The min value is: ",min(readings))
+print("The average value is: ", round(sum(readings)/5,2))
---
+++ test_mujoco.py
@@ -0,0 +1,20 @@
+import mujoco
+import mujoco.viewer
+
+xml = """
+<mujoco>
+  <worldbody>
+    <body pos="0 0 1">
+      <geom type="sphere" size="0.1"/>
+    </body>
+  </worldbody>
+</mujoco>
+"""
+
+model = mujoco.MjModel.from_xml_string(xml)
+data = mujoco.MjData(model)
+
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    while viewer.is_running():
+        mujoco.mj_step(model, data)
+        viewer.sync()
