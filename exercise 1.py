44ba5ea0635a09b72761f14f95b85e2929bf5920
Trần Nam Minh Việt <trannammv@gmail.com>
2026-04-26 10:00:57 +0700

    d

--- exercise 1.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-# Example data for testing purposes
-data = np.array([0.1, 0.2, 9.8, 0.15, 0.22, 9.78, 0.12, 0.21, 9.81, 0.11, 0.23, 9.79])
-# Reshape the data into a 2D array with 3 columns (x, y, z)
-reshaped_data = data.reshape(-1, 3)
